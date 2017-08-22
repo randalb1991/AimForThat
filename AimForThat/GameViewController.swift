8a1883a6ef7635a736d65135f0f20e49fb649028
Daniel Barrientos <rd.barrientosa@gmail.com>
2017-08-22 18:22:03 +0200

    First button and alert message

--- AimForThat/GameViewController.swift
+++ AimForThat/GameViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GameViewController: UIViewController {
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hola mundo", message: "Es mi primera alerta", preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "Genial", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
