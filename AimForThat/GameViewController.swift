545b0d19275f0debcb01522d430664e43ffcfc18
Daniel Barrientos <rd.barrientosa@gmail.com>
2017-08-23 00:55:21 +0200

    Score calculated and reset button working

--- AimForThat/GameViewController.swift
+++ AimForThat/GameViewController.swift
@@ -10,29 +10,59 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBAction func resetGame(_ sender: UIButton) {
+        self.reset()
+        self.startNewRound()
+        self.updateLabels()
+
+    }
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
     var targetValue: Int = 0
+    var round: Int = 0
+    var score: Int = 0
     
     @IBAction func sliderMoved(_ sender: UISlider) {
         //Solo puede ser llamado desde un UISlider
-        self.currentValue = Int(sender.value)
-        print("el valor del slider es \(Int(sender.value))")
-        
+        self.currentValue = Int(sender.value)        
     }
     
     @IBAction func showAlert() {
+        
+        let difference = abs(self.targetValue - self.currentValue)
+        /*
+        lineal
+        let points = 100 - difference
+        */
+        
+        let point = (difference > 0) ? 100 - difference : 1000
         let message = """
-        El valor del slider es: \(self.currentValue)
-        El valor del objetivo es \(self.targetValue)
+        Has marcado \(point) puntos!
         """
-        let alert = UIAlertController(title: "Puntuación", message: message, preferredStyle: .actionSheet)
+        
+        let title: String
+        
+        switch difference {
+        case 0:
+            title = "Puntuación Perfecta!"
+        case 1...5:
+            title = "Puntuación casi perfecta!"
+        case 6...12:
+            title = "No está mal!"
+        default:
+            title = "Afina tu punteria"
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Genial", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        self.score += point
         self.startNewRound()
         self.updateLabels()
     }
@@ -45,6 +75,7 @@ class GameViewController: UIViewController {
     }
 
     func startNewRound(){
+        self.round += 1
         self.targetValue = 1 + Int(arc4random_uniform(UInt32(self.slider.maximumValue)))
         self.currentValue = Int(self.slider.maximumValue)/2
         self.slider.value = Float(self.currentValue)
@@ -57,6 +88,18 @@ class GameViewController: UIViewController {
 
     func updateLabels(){
         self.targetLabel.text = "\(self.targetValue)"
+        self.scoreLabel.text = "\(self.score)"
+        self.roundLabel.text = "\(self.round)"
+    }
+    
+    func reset(){
+        let alert =  UIAlertController(title: "Juego Reseteado", message: nil, preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "OK", style: .default , handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        self.currentValue = 0
+        self.score = 0
+        self.round = 0
     }
 }
 
